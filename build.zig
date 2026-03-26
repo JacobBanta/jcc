106a7b65bd756af05d7950fb9fb331f70101659c
Jacob Banta <jacob@banta.email>
2026-03-26 00:11:06 -0400

    build pipeline improvements

--- build.zig
+++ build.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 
-pub fn build(b: *std.Build) void {
+pub fn build(b: *std.Build) !void {
     const optimize = b.standardOptimizeOption(.{});
     const target = b.standardTargetOptions(.{});
     const mod = b.createModule(.{
@@ -14,6 +14,19 @@ pub fn build(b: *std.Build) void {
     });
     b.installArtifact(exe);
     const cmd = b.addRunArtifact(exe);
-    const run = b.step("run", "Run jcc");
-    run.dependOn(&cmd.step);
+    cmd.has_side_effects = true;
+    cmd.stdio_limit = .unlimited;
+    const @"asm" = cmd.captureStdOut(.{ .basename = "a.asm" });
+    const nasm = b.addSystemCommand(&.{ "nasm", "-felf64", "-o" });
+    const obj = nasm.addOutputFileArg("a.o");
+    nasm.addFileArg(@"asm");
+    const compile = b.step("compile", "Compile test.c");
+    compile.dependOn(&nasm.step);
+    const ld = b.addSystemCommand(&.{ "ld", "-o" });
+    const out = ld.addOutputFileArg("a.out");
+    ld.addFileArg(obj);
+    const testc = b.addSystemCommand(&.{ "sh", "-c" });
+    testc.addFileArg(out);
+    const run = b.step("run", "Run test.c");
+    run.dependOn(&testc.step);
 }
